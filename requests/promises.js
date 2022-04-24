f21608f2ce54f85c8e2910681b774719e6eb2c4f
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-04-24 18:17:03 -0500

    Updating promises

--- requests/promises.js
+++ requests/promises.js
@@ -12,27 +12,72 @@ btnClick.onclick = () => {
 
 btnSlow.onclick = () => getUserInfo(1);
 
+// const getUserInfo = (id) => {
+//   fetch(`${url}/users?id=${id}`)
+//     .then(res => res.json())
+//     .then(users => content.innerHTML += `<h3>User Info</h3><p>${users[0].email}</p>`)
+//     .then(() => fetch(`${url}/posts?userId=${id}&_limit=3`))
+//     .then(res => res.json())
+//     .then(posts => {
+//       content.innerHTML += posts.map(post =>
+//         `<div class="post"><h4>${post.title}</h4></div>`
+//       ).join('');
+//       return Promise.all(posts.map(post => 
+//         fetch(`${url}/comments?postId=${post.id}&_limit=2`)
+//       ));
+//     })
+//     .then(responses => Promise.all(responses.map(res => res.json())))
+//     .then(comments => {
+//       document.querySelectorAll('.post').forEach((post, i) => {
+//         post.innerHTML += comments[i].map(comment => 
+//           `<p><span>${comment.email}</span>: ${comment.body}</p>`
+//         ).join('');
+//       });
+//     })
+//     .catch(err => console.log(err));
+// }
+
+const getUser = (id) => (
+  fetch(`${url}/users?id=${id}`)
+    .then(res => res.json())
+    .then(users => {
+      content.innerHTML += `<h3>User Info</h3><p>${users[0].email}</p>`
+      return users[0]
+    })
+)
+
+const getPosts = (user) => {
+  return fetch(`${url}/posts?userId=${user.id}&_limit=3`)
+    .then(res => res.json())
+    .then(posts => {
+      content.innerHTML += posts.map(post =>
+        `<div class="post"><h4>${post.title}</h4></div>`  
+      ).join('')
+
+      return posts;
+    })
+}
+
+const getComments = (posts) => {
+  Promise.all(posts.map(post => 
+    fetch(`${url}/comments?postId=${post.id}&_limit=2`)  
+  ))
+    .then(responses => Promise.all(responses.map(res => res.json())))
+    .then(comments => {
+      console.log(comments);
+      document.querySelectorAll(".post").forEach((div, i) => {
+        div.innerHTML += comments[i].map(comment =>
+          `<p><span>${comment.email}</span>: ${comment.body}</p>`  
+        ).join('')
+
+        return comments;
+      })
+    })
+}
+
 const getUserInfo = (id) => {
-	fetch(`${url}/users?id=${id}`)
-		.then(res => res.json())
-		.then(users => content.innerHTML += `<h3>User info</h3><p>${users[0].email}</p>`)
-		.then(() => fetch(`${url}/posts?userId=${id}&_limit=3`))
-		.then(res => res.json())
-		.then(posts => {
-			content.innerHTML += posts.map((post) =>
-				`<div class="post"><h4>${post.title}</h4></div>`
-			).join('')
-			return Promise.all(posts.map((post) =>
-				fetch(`${url}/comments?postId=${post.id}&_limit=2`)
-			))
-		})
-		.then(responses => Promise.all(responses.map(res => res.json())))
-		.then(comments => {
-			document.querySelectorAll(".post").forEach((div, i) => {
-				div.innerHTML += comments[i].map(comment => 
-					`<p><span>${comment.email}: ${comment.body}</span></p>`
-				).join();
-			})
-		})
-		.catch(e => console.error(e));
-};
+  getUser(id)
+    .then(user => getPosts(user))
+    .then(posts => getComments(posts))
+    .catch(e => console.log(e))
+}
